17284e260c9d3794ec68c54094a6957fc5904cdd
Andrzej Sikorski <andrzejstudia@gmail.com>
2018-11-12 14:51:02 +0100

    Tutorial 1, with mistake

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,49 @@
 package com.company;
 
-public class Main {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.sql.Driver;
+import java.util.stream.IntStream;
 
-    public static void main(String[] args) {
-	System.out.println("");
+public class Main {
+static int NUMB_OF_EPOCHS = 10000;
+static double TRAINING_DATA[][][] = new double[][][] {{{0,0}, {0}},
+                                                      {{0,1}, {1}},
+                                                      {{1,0}, {1}},
+                                                      {{1,1}, {0}}};
+    public static void main(String[] args)throws IOException {
+	    NeuralNetwork neuralNetwork = new NeuralNetwork();
+	    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        boolean flag = true;
+        while(flag) {
+            System.out.println("> What do you want to do (run, train, exit) ?");
+            String command = bufferedReader.readLine();
+            switch (command){
+                case "run":
+                    double[] result = new double[]{0,0,0,0};
+                    IntStream.range(0, Main.TRAINING_DATA.length).forEach(i-> {
+                        result[i] = neuralNetwork.forwardprop(Main.TRAINING_DATA[i][0]).getNeurons()[NeuralNetwork.INPUT_NEURONS+NeuralNetwork.HIDDEN_NEURONS].getOutput();
+                    });
+                    printResult(result);
+                    break;
+                case "train":
+                    IntStream.range(0, NUMB_OF_EPOCHS).forEach(i->{
+                        System.out.println("[epoch "+i+"]");
+                        IntStream.range(0, TRAINING_DATA.length).forEach(j-> System.out.println(neuralNetwork.forwardprop(Main.TRAINING_DATA[j][0]).backpropError(Main.TRAINING_DATA[j][1][0])));
+                    });
+                    System.out.println("[done training]");
+                    break;
+                case "exit":
+                    flag = false;
+                    break;
+            }
+        }
+        System.exit(0);
+    }
+    static void printResult(double[] result){
+        System.out.println("    Input1  |   Input2  |   Target Result   |   Result");
+        System.out.println("------------------------------------------------------");
+        IntStream.range(0, TRAINING_DATA.length).forEach(i->{IntStream.range(0, TRAINING_DATA[0][0].length).forEach(j->System.out.print("   "+TRAINING_DATA[i][0][j] + "    |   ")); System.out.print(" "+TRAINING_DATA[i][1][0]+"  |   "+String.format("%.5f", result[i])+"    \n");});
     }
 }
--- src/com/company/NeuralNetwork.java
+++ src/com/company/NeuralNetwork.java
@@ -1,16 +1,69 @@
 package com.company;
 
+import java.util.Arrays;
 import java.util.stream.IntStream;
 
 public class NeuralNetwork {
     static enum LayerType {I, H, O}
-    final static  int INPUT_NEURONS = 3;
-    final static int HIDDEN_NEURONS = 3;
-    final static int OUTPUT_NEURONS = 3;
+    final static  int INPUT_NEURONS = 2;
+    final static int HIDDEN_NEURONS = 2;
+    final static int OUTPUT_NEURONS = 1;
+    static final double LEARNING_RATE = 0.8;
     private Neuron[] neurons = new Neuron [INPUT_NEURONS + HIDDEN_NEURONS + OUTPUT_NEURONS];
     public NeuralNetwork(){
         IntStream.range(0, INPUT_NEURONS).forEach(i -> neurons[i] = new Neuron(LayerType.I));
         IntStream.range(INPUT_NEURONS, INPUT_NEURONS + HIDDEN_NEURONS).forEach(i -> neurons[i] = new Neuron(LayerType.H));
         neurons[INPUT_NEURONS + HIDDEN_NEURONS] = new Neuron(LayerType.O);
     }
+
+    public Neuron [] getNeurons() {
+        return neurons;
+    }
+
+    @Override
+    public String toString() {
+        return "NeuralNetwork{" +
+                "neurons=" + Arrays.toString(neurons) +
+                '}';
+    }
+
+    public NeuralNetwork forwardprop (double input[]){
+        double weightedSum = 0;
+        for (int i=0; i<neurons.length; i++){
+            switch (neurons[i].getLayerType()){
+                case I:
+                    neurons[i].setOutput(input[i]);
+                    break;
+                case H:
+                    weightedSum = neurons[i].getThreshold()+neurons[i].getWeights()[0]*neurons[0].getOutput() + neurons[i].getWeights()[1]*neurons[1].getOutput();
+                    neurons[i].applyActivationFunction(weightedSum);
+                    break;
+                case O:
+                    weightedSum = neurons[i].getThreshold()+neurons[i].getWeights()[0]*neurons[2].getOutput()+neurons[i].getWeights()[1]*neurons[3].getOutput();
+                    neurons[i].applyActivationFunction(weightedSum);
+                    break;
+            }
+        }
+        return this;
+    }
+
+    public NeuralNetwork backpropError(double targetResult){
+
+        neurons[4].setError((targetResult-neurons[4].getOutput())*neurons[4].derivative());
+        neurons[4].setThreshold(neurons[4].getThreshold() + LEARNING_RATE*neurons[4].getError());
+        neurons[4].getWeights()[0] = neurons[4].getWeights()[0]+LEARNING_RATE*neurons[4].getError()*neurons[2].getOutput();
+        neurons[4].getWeights()[1] = neurons[4].getWeights()[1]+LEARNING_RATE*neurons[4].getError()*neurons[3].getOutput();
+
+        neurons[3].setError((neurons[4].getWeights()[1]*neurons[4].getError())*neurons[3].derivative());
+        neurons[3].setThreshold(neurons[3].getThreshold()+LEARNING_RATE*neurons[3].getError());
+        neurons[3].getWeights()[0] = neurons[3].getWeights()[0]+LEARNING_RATE*neurons[3].getError()*neurons[0].getOutput();
+        neurons[3].getWeights()[1] = neurons[3].getWeights()[1]+LEARNING_RATE*neurons[3].getError()*neurons[1].getOutput();
+
+        neurons[2].setError((neurons[4].getWeights()[0]*neurons[4].getError())*neurons[2].derivative());
+        neurons[2].setThreshold(neurons[2].getThreshold()+LEARNING_RATE*neurons[2].getError());
+        neurons[2].getWeights()[0] = neurons[2].getWeights()[0]+LEARNING_RATE*neurons[2].getError()*neurons[0].getOutput();
+        neurons[2].getWeights()[1] = neurons[2].getWeights()[1]+LEARNING_RATE*neurons[2].getError()*neurons[1].getOutput();
+        return this;
+    }
+
 }
